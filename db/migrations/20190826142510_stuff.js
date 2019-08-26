687fe3666ac45442563cf71374d21cf91a4a5f7a
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-08-26 14:43:35 -0500

    removed restricted from posts for testing

--- db/migrations/20190826142510_stuff.js
+++ db/migrations/20190826142510_stuff.js
@@ -1,8 +1,16 @@
-
 exports.up = function(knex) {
-  
+	return knex.schema.createTable('stuff', (stuff => {
+		stuff.increments();
+
+		stuff.text('username', 200).notNullable().unique();
+        stuff.text('email', 200).notNullable().unique();
+        stuff.text('item', 50).notNullable();
+        stuff.text('description', 4000);
+        stuff.text('price', 100).notNullable();
+        
+	});
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('stuff');
 };
