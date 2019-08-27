3ca2b30263abaa198ee1a7ce403beed83d1d906e
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-08-27 15:41:16 -0500

    Added new fields to the stuff table

--- db/migrations/20190826142510_stuff.js
+++ db/migrations/20190826142510_stuff.js
@@ -2,12 +2,22 @@ exports.up = function(knex) {
 	return knex.schema.createTable('stuff', (stuff => {
 		stuff.increments();
 
-		stuff.text('username', 200).notNullable().unique();
-        stuff.text('email', 200).notNullable().unique();
-        stuff.text('item', 50).notNullable();
+		stuff.text('username', 200).unique();
+        stuff.text('email', 200).unique();
+        stuff.text('item', 50);
         stuff.text('description', 4000);
-        stuff.text('price', 100).notNullable();
-        
+		stuff.text('price', 100)
+		stuff.text('category',100);
+		stuff.text('product-lifetime',200);
+		stuff.text('model-num',35);
+		stuff.text('features-1',2000);
+		stuff.text('features-2',2000);
+		stuff.text('features-3',2000);
+		stuff.text('features-4',2000);
+		stuff.text('features-5',2000);
+		stuff.text('status',2000);
+		stuff.text('warranty',200);
+		stuff.text('sku-num',2000)
 	}));
 };
 
