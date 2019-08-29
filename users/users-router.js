810e43ffb1239a977820024ffbec621b1701fc20
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-08-28 20:21:55 -0500

    Added some more user endpoints

--- users/users-router.js
+++ users/users-router.js
@@ -12,5 +12,28 @@ router.get('/', restricted, (req, res) => {
 			res.status(404).json(err);
 		});
 });
+router.delete('/:id', restricted, (req,res) => {
+	const { id } = req.params;
+	Users.deleteUser()
+	.then((users) => {
+		res.status(202).json(users)
+	})
+	.catch((err) => {
+		res.status(500).json({message:"yeet it failed delete"})
+	})
+})
+
+router.put('/:id',restricted, (req,res) => {
+	const body = req.body;
+	const { id } = req.params;
+	
+	Users.updateUser(body,id)
+	.then((users) => {
+		res.status(200).json(users)
+	})
+	.catch((err) => {
+		res.status(500).json({message:"Put got shook and didn't put"})
+	})
+})
 
 module.exports = router;
